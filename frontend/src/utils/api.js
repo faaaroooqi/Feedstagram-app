2df4c70af02e57e2c8cfe32f7a09ee3cdd5844e9
faaaroooqi <i201788@nu.edu.pk>
2025-10-07 12:49:19 +0500

    ...

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-   baseUrL:import.meta.env.VITE_API_URL, // backend base URL
+   baseURL:import.meta.env.VITE_API_URL, // backend base URL
 });
 
 // Attach JWT token automatically if available
